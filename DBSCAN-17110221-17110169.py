29be7c57a459334393f2cfd6cf718675e0af7cd0
yasuo99 <yasuo12091999@gmail.com>
2021-01-02 13:31:44 +0700

    Cap nhat code va dataset

--- DBSCAN-17110221-17110169.py
+++ DBSCAN-17110221-17110169.py
@@ -7,6 +7,7 @@ Created on Wed Nov 18 15:21:00 2020
 import numpy as np 
 import pandas as pd 
 import matplotlib.pyplot as plt 
+import seaborn as sns
 
 from sklearn.cluster import DBSCAN 
 from sklearn.preprocessing import StandardScaler 
@@ -14,109 +15,102 @@ from sklearn.preprocessing import normalize
 from sklearn.decomposition import PCA 
 from sklearn.metrics import silhouette_samples, silhouette_score
 from sklearn.cluster import KMeans
+from scipy.stats import pearsonr
+from itertools import product
 
+class Dbscan: 
+    df = pd.DataFrame()
+    def __init__(self, X_numerics):
+            self.df = X_numerics
+    def DetermineEpsAndMinSamples(self):
+        eps_values = np.arange(8,12.75,0.25) # eps values to be investigated
+        min_samples = np.arange(3,10) # min_samples values to be investigated
+        DBSCAN_params = list(product(eps_values, min_samples))
 
-X = pd.read_csv('Mall_Customers.csv') 
+        no_of_clusters = []
+        sil_score = []
 
+        for p in DBSCAN_params:
+            DBS_clustering = DBSCAN(eps=p[0], min_samples=p[1]).fit(X_numerics)
+            no_of_clusters.append(len(np.unique(DBS_clustering.labels_)))
+            sil_score.append(silhouette_score(X_numerics, DBS_clustering.labels_))
+
+        tmp = pd.DataFrame.from_records(DBSCAN_params, columns =['Eps', 'Min_samples'])   
+        tmp['No_of_clusters'] = no_of_clusters
+
+        pivot_1 = pd.pivot_table(tmp, values='No_of_clusters', index='Min_samples', columns='Eps')
+
+        fig, ax = plt.subplots(figsize=(12,6))
+        sns.heatmap(pivot_1, annot=True,annot_kws={"size": 16}, cmap="YlGnBu", ax=ax)
+        ax.set_title('Number of clusters')
+        plt.show()
+        #Dựa vào heatplot trên, số lượng clusters nằm trong khoảng 17 - 4.
+        tmp = pd.DataFrame.from_records(DBSCAN_params, columns =['Eps', 'Min_samples'])   
+        tmp['Sil_score'] = sil_score
+
+        pivot_1 = pd.pivot_table(tmp, values='Sil_score', index='Min_samples', columns='Eps')
+
+        fig, ax = plt.subplots(figsize=(18,6))
+        sns.heatmap(pivot_1, annot=True, annot_kws={"size": 10}, cmap="YlGnBu", ax=ax)
+        plt.show()
+        #Silhouette score cao nhất là 0.26 với eps=12.5 và min_samples=4.
+    def Clustering(self, eps, min_samples):
+        DBS_clustering = DBSCAN(eps=eps, min_samples=min_samples).fit(self.df)
+        DBSCAN_clustered = self.df.copy()
+        DBSCAN_clustered.loc[:,'Cluster'] = DBS_clustering.labels_ # append labels to points
+
+        DBSCAN_clust_sizes = DBSCAN_clustered.groupby('Cluster').size().to_frame()
+        DBSCAN_clust_sizes.columns = ["DBSCAN_size"]
+        print(DBSCAN_clust_sizes)
+        return DBSCAN_clustered
+        
+mall_data = pd.read_csv('Mall_Customers.csv') 
+print(mall_data.describe());
 # Bỏ cột mã khách hàng khỏi dataset
-X = X.drop('CustomerID', axis = 1) 
-
+X = mall_data.drop('CustomerID', axis = 1) 
+# Bỏ cột giới tính ra khỏi dataset
+X = X.drop('Gender', axis = 1) 
 # Xử lý dữ liệu NaN
 X.fillna(method ='ffill', inplace = True) 
 
 print(X.head()) 
+corr, _ = pearsonr(mall_data['Age'], mall_data['Spending Score (1-100)'])
 
-# Scaling dữ liệu
-scaler = StandardScaler() 
-X_scaled = scaler.fit_transform(X) 
+jp = (sns.jointplot('Age', 'Spending Score (1-100)', data=mall_data,
+                    kind='reg')).plot_joint(sns.kdeplot, zorder=0, n_levels=6)
 
-# Bình thường hóa dữ liệu
-X_normalized = normalize(X_scaled) 
-
-# Chuyển từ numpy array thành dataframe
-X_normalized = pd.DataFrame(X_normalized) 
-
-pca = PCA(n_components = 2) 
-X_principal = pca.fit_transform(X_normalized) 
-X_principal = pd.DataFrame(X_principal) 
-X_principal.columns = ['P1', 'P2'] 
-
-
-plt.scatter(X_principal['P1'], X_principal['P2'])
+plt.text(0,120, 'Pearson: {:.2f}'.format(corr))
 plt.show()
 
-X_principal = X_principal.sort_values(by=['P1','P2']);
+#Tạo dataset chứa các mẫu data để phân cụm
+X_numerics = mall_data[['Age', 'Annual Income (k$)', 'Spending Score (1-100)']]
+dbscan = Dbscan(X_numerics)
+#Xác định eps và min_samples
 
-df2 = pd.DataFrame(columns = ['index','distance'])
-for i in range(0,len(X_principal) - 1):
-    dist = np.linalg.norm(X_principal.iloc[i] - X_principal.iloc[i+1])
-    df2 = df2.append({'index': str(i), 'distance': dist}, ignore_index=True)
+dbscan.DetermineEpsAndMinSamples()
     
-df2 = df2.sort_values(by=['distance'])
-plt.scatter(df2['index'], df2['distance'])
-plt.title('Biểu đồ thể hiện khoảng cách giữa các điểm')
-plt.show();
-print("Tính toán")
-df3 = df2[df2['distance'] < 0.5]
-df3 = df3.sort_values(by=['distance'])
-plt.scatter(df3['index'], df3['distance'])
-plt.title('Biểu đồ thể hiện khoảng cách giữa các điểm mà có khoảng cách < 0.5')
-plt.show();
+#Gom cụm với eps = 12.5 và min_samples = 4
 
-#Lua chon eps tot nhat
-# range_eps = [0.1,0.2,0.3,0.4,0.5]
-# for i in range_eps:
-#     print("eps value is: " + str(i))
-#     db = DBSCAN(eps=i, min_samples=5).fit(X_principal)
-#     core_sample_mask = np.zeros_like(db.labels_, dtype=bool)
-#     core_sample_mask[db.core_sample_indices_] = True
-#     labels = db.labels_
-#     silhouette_avg = silhouette_score(X_principal,labels)
-#     print("For eps value = "+str(i), "Diem trung binh silhouette_score la: ",silhouette_avg)
+DBSCAN_clustered = dbscan.Clustering(eps = 12.5, min_samples = 4)
+#visualize cluster
+outliers = DBSCAN_clustered[DBSCAN_clustered['Cluster']==-1]
 
-# #Lua chon so diem toi thieu cua 1 cluster
-# min_samples = [1,2,3,4,5,6,7,8,9,10]
-# for i in min_samples:
-#     db = DBSCAN(eps=0.1, min_samples=i).fit(X_principal)
-#     core_sample_mask = np.zeros_like(db.labels_, dtype=bool)
-#     core_sample_mask[db.core_sample_indices_] = True
-#     labels = set([label for label in db.labels_ if label >= 0])
-#     print("voi so diem toi thieu la " + str(i), "So cluster la: " + str(len(set(labels))))
+fig2, (axes) = plt.subplots(1,2,figsize=(12,5))
 
-db_default = DBSCAN(eps = 0.1, min_samples = 7).fit(X_principal) 
-labels = db_default.labels_ 
+#Visualize theo thu nhập và % sử dụng
+sns.scatterplot('Annual Income (k$)', 'Spending Score (1-100)',
+                data=DBSCAN_clustered[DBSCAN_clustered['Cluster']!=-1],
+                hue='Cluster', ax=axes[0], palette='Set1', legend='full', s=45)
+#Visualize theo tuổi và % sử dụng
+sns.scatterplot('Age', 'Spending Score (1-100)',
+                data=DBSCAN_clustered[DBSCAN_clustered['Cluster']!=-1],
+                hue='Cluster', palette='Set1', ax=axes[1], legend='full', s=45)
 
-n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
-n_noise_ = list(labels).count(-1)
+axes[0].scatter(outliers['Annual Income (k$)'], outliers['Spending Score (1-100)'], s=5, label='outliers', c="k")
+axes[1].scatter(outliers['Age'], outliers['Spending Score (1-100)'], s=5, label='outliers', c="k")
+axes[0].legend()
+axes[1].legend()
+plt.setp(axes[0].get_legend().get_texts(), fontsize='10')
+plt.setp(axes[1].get_legend().get_texts(), fontsize='10')
 
-print('Ước tính số cluster là: %d' % n_clusters_)
-print('Ước tính số điểm nhiễu là: %d' % n_noise_)
-
-colours = {} 
-colours[0] = 'r'
-colours[1] = 'g'
-colours[2] = 'b'
-colours[3] = 'yellow'
-colours[4] = 'purple'
-colours[-1] = 'k'
-
-
-cvec = [colours[label] for label in labels] 
-
-
-r = plt.scatter(X_principal['P1'], X_principal['P2'], color ='r'); 
-g = plt.scatter(X_principal['P1'], X_principal['P2'], color ='g'); 
-b = plt.scatter(X_principal['P1'], X_principal['P2'], color ='b'); 
-k = plt.scatter(X_principal['P1'], X_principal['P2'], color ='k'); 
 plt.show()
-
-
-plt.figure(figsize =(9, 9)) 
-plt.scatter(X_principal['P1'], X_principal['P2'], c = cvec) 
-
-plt.legend((r, g, b, y,k), ('Nữ dưới 30 dùng trên 100', 'Nam dưới 30 dùng trên 100', 'Nam trên 30 dùng trên 200','Nữ trên 30 dùng trên 200', 'Còn lại'),scatterpoints = 1, 
-		loc = 3, 
-		ncol = 3, 
-		fontsize = 8) 
-plt.title('Biểu đồ thể hiện mức chi tiêu và thu nhập của nam và nữ ở độ tuổi từ 20 - 60')
-plt.show() 
